d987a8c417092985dda4778490609b3b5eb73193
richearda <richearda1996@gmail.com>
2024-04-07 21:01:06 +0800

    add scoped service for repository interfaces

--- TicketingSystem.Persistence/PersistenceServiceRegistration.cs
+++ TicketingSystem.Persistence/PersistenceServiceRegistration.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TicketingSystem.Application.Contracts.Persistence;
+using TicketingSystem.Persistence.Repositories;
 
 namespace TicketingSystem.Persistence
 {
@@ -14,6 +16,13 @@ namespace TicketingSystem.Persistence
         public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration) 
         {
             services.AddDbContext<TicketingSystemDBContext>(options => options.UseSqlServer(configuration.GetConnectionString("TicketingSystemConnection")));
+
+            services.AddScoped<ICompanyRepository, CompanyRepository>();
+            services.AddScoped<ICostCenterRepository, CostCenterRepository>();
+            services.AddScoped<IRoleRepository, RoleRepository>();
+            services.AddScoped<ISystemInfoRepository, SystemInfoRepository>();
+            services.AddScoped<ITicketRepository, TicketRepository>();
+
 
             return services;
         }
