57f6ab484fd0bfef2f9d291e66aca77b6d9b924d
richearda <richearda1996@gmail.com>
2024-04-08 22:24:16 +0800

    Added mapping profiles

--- TicketingSystem.Application/Profiles/MappingProfile.cs
+++ TicketingSystem.Application/Profiles/MappingProfile.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TicketingSystem.Application.DTOs;
+using TicketingSystem.Domain;
 
 namespace TicketingSystem.Application.Profiles
 {
-    public class MappingProfile
+    public class MappingProfile:Profile
     {
+        public MappingProfile()
+        {
+            CreateMap<Attachment, AttachmentDto>().ReverseMap();
+            CreateMap<Company, CompanyDto>().ReverseMap();
+            CreateMap<CostCenter, CostCenterDto>().ReverseMap();
+            CreateMap<Role, RoleDto>().ReverseMap();
+            CreateMap<SystemInfo, SystemInfoDto>().ReverseMap();
+            CreateMap<Ticket, TicketDto>().ReverseMap();
+        }
+        
     }
 }
