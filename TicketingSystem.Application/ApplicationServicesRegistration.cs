dcbc395292ecddd69161e4dbaed1b58f37f84fa3
richearda <richearda1996@gmail.com>
2024-04-09 22:46:56 +0800

    Register mediatr service

--- TicketingSystem.Application/ApplicationServicesRegistration.cs
+++ TicketingSystem.Application/ApplicationServicesRegistration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace TicketingSystem.Application
         public static IServiceCollection ConfigureApplicationService(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
             return services;
         }
     }
