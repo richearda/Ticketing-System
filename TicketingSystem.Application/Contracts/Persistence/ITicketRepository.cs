ee581f4aafa08c2bb684af452d0852bae68c2f18
richearda <richearda1996@gmail.com>
2024-04-29 20:03:25 +0800

    Replace return type with DTO's

--- TicketingSystem.Application/Contracts/Persistence/ITicketRepository.cs
+++ TicketingSystem.Application/Contracts/Persistence/ITicketRepository.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TicketingSystem.Application.DTOs;
+using TicketingSystem.Application.DTOs.Ticket;
 using TicketingSystem.Domain;
 
 namespace TicketingSystem.Application.Contracts.Persistence
@@ -17,37 +19,37 @@ namespace TicketingSystem.Application.Contracts.Persistence
          /// </summary>
          /// <param name="userId">ID of assignor</param>
          /// <returns>List of ticket</returns>
-        Task<List<Ticket>> GetTicketsAssignedBy(int userId);
+        Task<List<GetTicketsByAssignorDto>> GetTicketsAssignedBy(int userId);
         /// <summary>
         /// Returns the tickets that has been assigned to the user
         /// </summary>
         /// <param name="userId">ID of assignee</param>
         /// <returns>List of ticket</returns>
-        Task<List<Ticket>> GetTicketsAssignedTo(int userId);
+        Task<List<GetTicketsByAssignee>> GetTicketsAssignedTo(int userId);
         /// <summary>
         /// Returns the tickets by requestor
         /// </summary>
         /// <param name="userId">ID of the requestor</param>
         /// <returns>List of ticket</returns>
-        Task<List<Ticket>> GetTicketsRequestedBy(int userId);
+        Task<List<GetTicketsByRequestorDto>> GetTicketsRequestedBy(int userId);
         /// <summary>
         /// Returns the tickets by date requested
         /// </summary>
         /// <param name="requestedDate">Date of request</param>
         /// <returns>List of ticket</returns>
-        Task<List<Ticket>> GetTicketsByRequestedDate(DateOnly requestedDate);
+        Task<List<GetTicketsByDateDto>> GetTicketsByRequestedDate(DateOnly requestedDate);
         /// <summary>
         /// Returns the tickets by due date
         /// </summary>
         /// <param name="dueDate">Due date</param>
         /// <returns>List of ticket</returns>
-        Task<List<Ticket>> GetTicketsByDueDate(DateOnly dueDate);
+        Task<List<GetTicketsByDateDto>> GetTicketsByDueDate(DateOnly dueDate);
         /// <summary>
         /// Returns the ticket with attachement details
         /// </summary>
         /// <param name="ticketId">ID of the ticket</param>
         /// <returns>Returna a ticket</returns>
-        Task<Ticket> GetTicketWithAttachmentDetails(int ticketId);
+        Task<TicketDto> GetTicketWithAttachmentDetails(int ticketId);
 
     }
 }
