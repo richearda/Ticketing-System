eb25d8439a9ede15e88dcd7770cdaababd335738
richearda <richearda1996@gmail.com>
2024-05-21 21:38:01 +0800

    Remove incorrect reference

--- TicketingSystem.Application/DTOs/SystemInfo/SystemInfoDto.cs
+++ TicketingSystem.Application/DTOs/SystemInfo/SystemInfoDto.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TicketingSystem.Application.DTOs.CostCenter;
 using TicketingSystem.Domain;
 
 namespace TicketingSystem.Application.DTOs
